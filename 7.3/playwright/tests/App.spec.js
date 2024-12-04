2bfb89b2dbe7cedcba1020259d6e458f8195a610
MikhailD <proxdme@mail.ru>
2024-12-04 12:55:01 +0300

    Добавлены тесты для авторизации с Playwright

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,22 +1,39 @@
-const { test, expect } = require("@playwright/test");
+const { test, expect } = require('@playwright/test');
+const { email, password } = require('../user.js');  // Импортируем данные из user.js
 
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
+// Тест 1: Успешная авторизация
+test('Успешная авторизация', async ({ page }) => {
+  // Открываем страницу авторизации
+  await page.goto('https://netology.ru/?modal=sign_in');
+  
+    // Вводим email и пароль
+  await page.fill('input[name="email"]', email);
+  await page.fill('input[name="password"]', password);
 
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
+  // Нажимаем на кнопку "Войти"
+  await page.click('[data-testid="login-submit-btn"]');
 
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
+  // Проверяем, что открылась страница профиля
+  const profileHeader = await page.locator('//h2[@class="src-components-pages-Profile-Programs--title--Kw5NH" and contains(text(), "Моё обучение")]'
+);
+  
+});
 
-  page.click("text=Бизнес и управление");
+// Тест 2: Неуспешная авторизация
+test('Неуспешная авторизация', async ({ page }) => {
+  // Открываем страницу авторизации
+  await page.goto('https://netology.ru/?modal=sign_in');
 
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
-  );
+  
+  // Вводим невалидные данные
+  await page.fill('input[name="email"]', 'invalid-email@example.com');
+  await page.fill('input[name="password"]', 'invalid-password');
+
+  // Нажимаем на кнопку "Войти"
+  await page.click('[data-testid="login-submit-btn"]');
+
+  // Проверяем наличие сообщения об ошибке
+  const errorMessage = await page.locator('[data-testid="login-error-hint"]');
+  await expect(errorMessage).toBeVisible();
+  await expect(errorMessage).toHaveText('Вы ввели неправильно логин или пароль.');
 });
